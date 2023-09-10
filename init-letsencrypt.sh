dd28744e192f1b4b10b6f88fd3da9d35d56514c8
Marcelo Souza <souzadeveloper@gmail.com>
2023-09-10 13:14:12 -0300

    Ajustes

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,19 +5,12 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(
-  refactorsistemas.com.br 
-  chat.refactorsistemas.com.br 
-  evolution.refactorsistemas.com.br 
-  minio.refactorsistemas.com.br 
-  n8n.refactorsistemas.com.br
-  storage.refactorsistemas.com.br
-  typebot.refactorsistemas.com.br)
+domains=(refactorsistemas.com.br chat.refactorsistemas.com.br evolution.refactorsistemas.com.br minio.refactorsistemas.com.br n8n.refactorsistemas.com.br storage.refactorsistemas.com.br typebot.refactorsistemas.com.br)
 
 rsa_key_size=4096
 data_path="./certbot_data"
 email="souzadeveloper@gmail.com" # Adding a valid address is strongly recommended
-staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
